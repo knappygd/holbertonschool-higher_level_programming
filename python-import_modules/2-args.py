60e043bfe0309fc75e10d44ae144122724fae255
knapp <emiliodamasco04@gmail.com>
2023-06-02 06:59:57 -0700

    fixed pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
-    
+
     for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
