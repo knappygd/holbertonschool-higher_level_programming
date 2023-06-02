68a73adfc719560124d946e02e44e823ffd62f05
knapp <emiliodamasco04@gmail.com>
2023-06-02 06:06:01 -0700

    fixed it working weird

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for i in range(100):
+    for i in range(1, 100):
         if i % 3 == 0:
             print("Fizz ", end="")
-        if i % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz ", end="")
-        if i % 3 == 0 and i % 5 == 0:
+        elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
-        print("{} ".format(i), end="")
+        else:
+            print("{} ".format(i), end="")
+
+fizzbuzz()
