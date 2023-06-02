49f9c3dce5e21ffebc2db5a0efb2b1bb9456e5ae
knapp <emiliodamasco04@gmail.com>
2023-06-02 06:17:30 -0700

    fixed newlines

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,8 @@ def uppercase(str):
     for c in str:
         if ord(c) > 96 and ord(c) < 123:
             c = chr(ord(c) - 32)
-            print("{}".format(c))
+            print("{}".format(c), end="")
         else:
-            print("{}".format(c))
+            print("{}".format(c), end="")
+
+uppercase("This is a Test")
