a64fc0007e338a29bd5f17d269a2ba6349ae88b3
knapp <emiliodamasco04@gmail.com>
2023-06-02 06:14:46 -0700

    fixed double print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,5 @@ def uppercase(str):
         if ord(c) > 96 and ord(c) < 123:
             c = chr(ord(c) - 32)
             print("{}".format(c))
-        print("{}".format(chr(c)))
+        else:
+            print("{}".format(chr(c)))
