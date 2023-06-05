33fed0741bf1f20578ac9d05b3cc8e1f60b2e927
knapp <emiliodamasco04@gmail.com>
2023-06-05 08:44:00 -0700

    fixed newline

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,6 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if j == len(matrix) - 1:
-                    print("{:d}\n".format(matrix[i][j]), end="")
+                    print("{:d}".format(matrix[i][j]), end="")
                 else:
                     print("{:d}".format(matrix[i][j]), end=" ")
+            print()
