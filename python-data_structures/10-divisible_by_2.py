08e276295b476a1820e2e6b74ceb04fab028592e
knapp <emiliodamasco04@gmail.com>
2023-06-06 05:49:25 -0700

    fixed list

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    new = []
+    new = [0] * len(my_list)
     for i in my_list:
         if my_list[i] % 2 == 0:
-            new.append(True)
+            new[i] = True
         else:
-            new.append(False)
+            new[i] = False
     return new
