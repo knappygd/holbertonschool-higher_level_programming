4084e5f8aae6bd2a27e3bb6f155b206d6c8ee541
knapp <emiliodamasco04@gmail.com>
2023-06-06 06:47:27 -0700

    added 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    new = [0] * len(my_list)
+
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new[i] = replace
+        else:
+            new[i] = my_list[i]
+    return new
