71537dcce896b4c6e075a97f7c6a83883cde3470
knapp <emiliodamasco04@gmail.com>
2023-06-07 06:04:41 -0700

    fixed return

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     ret = 0
     for i in range(x):
         try:
-            ret += 1
             print("{}".format(my_list[i]), end="")
+            ret += 1
         except IndexError:
             break
     print()
