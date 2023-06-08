3b64fcb89c14b1fbf964d137c5527e06751a190b
knapp <emiliodamasco04@gmail.com>
2023-06-08 09:11:55 -0700

    fixed return

--- python-more_data_structures/main.py
+++ python-more_data_structures/main.py
@@ -3,11 +3,11 @@
 """
 roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-roman_number = "X"
+roman_number = 9
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
 roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+print("{} = {}".format(roman_number, roman_to_int(None)))
 
 roman_number = "XIX"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
