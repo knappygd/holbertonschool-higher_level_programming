db378c7d6e85ca980ebbb74dea7a9e267f7429c3
knapp <emiliodamasco04@gmail.com>
2023-06-12 06:12:54 -0700

    fixed error msg

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,9 +9,10 @@ class Square:
     Args:
         size (int): The size of the square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         if isinstance(size, int) == False:
             raise TypeError()
         elif size < 0:
-            raise ValueError()
-        self.__size = size
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
