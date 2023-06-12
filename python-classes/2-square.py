189579a2425bddac53acf51eb831369b1df8feb0
knapp <emiliodamasco04@gmail.com>
2023-06-12 06:15:55 -0700

    fixed pycode

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         size (int): The size of the square
     """
     def __init__(self, size=0):
-        if isinstance(size, int) == False:
+        if not isinstance(size, int):
             raise TypeError()
         elif size < 0:
             raise ValueError("size must be >= 0")
