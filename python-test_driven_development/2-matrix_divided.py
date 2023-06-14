d51dc1c24540fa6dab64df7b404e0bc5e8005162
knapp <emiliodamasco04@gmail.com>
2023-06-14 07:42:21 -0700

    added 3

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
+    for i in matrix:
+        for j in matrix[i]:
+            if not isinstance(matrix[i][j], int)\
+                    and not isinstance(matrix[i][j], float):
+                raise TypeError("matrix must be a matrix (list of lists) \
+                                of integers/floats")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
     new = matrix.copy()
     for i in range(len(matrix)):
-        new[i] = list(map(lambda x: x / div, matrix[i]))
+        new[i] = list(map(lambda x: round((x / div), 2), matrix[i]))
     return new
