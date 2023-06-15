f7c31dcf8436c429609df8b9fbf256e6989c75b4
knapp <emiliodamasco04@gmail.com>
2023-06-15 07:08:47 -0700

    fixed returns

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Defines a rectangle"""
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -19,6 +19,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -30,3 +31,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
