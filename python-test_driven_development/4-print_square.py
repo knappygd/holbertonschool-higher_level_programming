a85e7ce7ef59a027fa6429518c89e9942728952f
knapp <emiliodamasco04@gmail.com>
2023-06-15 05:47:10 -0700

    added 4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+print_square prints a square of a given size
+"""
+
 
 def print_square(size):
+    """
+    Print a square of <size> dimension
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
