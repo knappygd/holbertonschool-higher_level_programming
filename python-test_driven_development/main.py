7c63dd0b60245f336b557d57fa5a71ca72d50c37
knapp <emiliodamasco04@gmail.com>
2023-06-15 05:32:40 -0700

    added 3

--- python-test_driven_development/main.py
+++ python-test_driven_development/main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-say_my_name = __import__('3-say_my_name').say_my_name
+print_square = __import__('4-print_square').print_square
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
+print_square(-1)
+print("")
 try:
-    say_my_name()
+    print_square(-1)
 except Exception as e:
     print(e)
+print("")
