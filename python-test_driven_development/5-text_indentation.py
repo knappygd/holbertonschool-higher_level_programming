76e14db0ddc6b91168bbd3f234e3ac0974e52f4a
knapp <emiliodamasco04@gmail.com>
2023-06-16 10:49:17 -0700

    added 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Print text with a line jump each specific character
+"""
+
+def text_indentation(text):
+    """
+    Print jumpline text
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    s = text[:]
+
+    for char in ".?:":
+        list = s.split(char)
+        s = ""
+        for i in list:
+            i = i.strip(" ")
+            s = i + char if s is "" else s + "\n\n" + i + char
+
+    print(s[:-3], end="")
