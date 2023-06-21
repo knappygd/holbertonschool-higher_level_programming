f1082fb4bc46fb12d3c759e47fd90240e62820a5
knapp <emiliodamasco04@gmail.com>
2023-06-21 06:36:58 -0700

    fixed blank lines

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,8 @@
 Defines a function to lookup the
 available attributes and methods of an object
 """
+
+
 def lookup(obj):
     """
     Returns a list of available attributes and methods of an object
