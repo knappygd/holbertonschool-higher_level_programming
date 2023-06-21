eb7d25cd5f054fe8653e9827b8010d603dc87b1a
knapp <emiliodamasco04@gmail.com>
2023-06-21 07:49:09 -0700

    fixed import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,22 +4,14 @@ Class that raises an exeption
 """
 
 
-class BaseGeometry:
+BaseGeometry = __import__('7-rectangle').BaseGeometry
+
+class Rectangle(BaseGeometry):
     """
-    Raises an Exception with the message area() is not implemented
+    Inherits from BaseGeometry and instantiates
     """
-
-    def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise TypeError("{} must be greater than 0".format(name))
+def __init__(self, width, height):
+    self.integer_validator("width", width)
+    self.__width = width
+    self.integer_validator("height", height)
+    self.__height = height
