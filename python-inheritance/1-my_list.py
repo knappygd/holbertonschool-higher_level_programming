7962590ccd8aa0fd09d6df3a4ec2ef171b3d070a
knapp <emiliodamasco04@gmail.com>
2023-06-21 06:58:06 -0700

    added 2

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Print a sorted list
+"""
+
+
+class MyList:
+    """
+    Prints a list of integers in ascending order
+    """
+    def print_sorted(self):
+        print(sorted(list))
