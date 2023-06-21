c1a4f1c81294d1091e9ec9e0c91327227e1f1306
knapp <emiliodamasco04@gmail.com>
2023-06-21 06:35:32 -0700

    fixed docs

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-
+"""
+Defines a function to lookup the
+available attributes and methods of an object
+"""
 def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object
+    """
     return dir(obj)
