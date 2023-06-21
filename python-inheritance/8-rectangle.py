ccaf9fdc5f42c649fe3e6b0fe3894fa1f5e2f992
knapp <emiliodamasco04@gmail.com>
2023-06-21 07:50:27 -0700

    fixed newlines

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,16 +2,16 @@
 """
 Class that raises an exeption
 """
-
-
 BaseGeometry = __import__('7-rectangle').BaseGeometry
+
 
 class Rectangle(BaseGeometry):
     """
     Inherits from BaseGeometry and instantiates
     """
-def __init__(self, width, height):
-    self.integer_validator("width", width)
-    self.__width = width
-    self.integer_validator("height", height)
-    self.__height = height
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
