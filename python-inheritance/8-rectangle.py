6d11808d42e1ca19b4420c3535e79826f4eaf833
knapp <emiliodamasco04@gmail.com>
2023-06-21 07:51:59 -0700

    fixed import file

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,7 +2,7 @@
 """
 Class that raises an exeption
 """
-BaseGeometry = __import__('7-base_rectangle').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
