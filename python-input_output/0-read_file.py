de45c71c6a455691f08b6b7f0079f6b9d636e51a
knapp <emiliodamasco04@gmail.com>
2023-06-21 10:44:58 -0700

    fied docs

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Reads a file
+"""
+
 
 def read_file(filename=""):
+    """
+    Prints the content of a file
+    """
     with open(filename, encoding="utf8") as file:
         print(file.read(), end="")
