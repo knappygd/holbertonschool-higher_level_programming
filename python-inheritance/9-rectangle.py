fb181a79f32a026424a6d80839637aef6e7fd1fa
knapp <emiliodamasco04@gmail.com>
2023-06-21 09:09:33 -0700

    added 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Class that raises an exeption
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Inherits from BaseGeometry and instantiates
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
