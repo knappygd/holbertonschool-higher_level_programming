2107aceac925c1b70d466dbbf7585aab95c43e86
knapp <emiliodamasco04@gmail.com>
2023-06-21 10:51:33 -0700

    fixed docs

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,6 +5,9 @@ Append a string at the end
 
 
 def append_write(filename="", text=""):
+    """
+    Append text at the end of a file
+    """
     with open(filename, "a") as file:
         file.write(text)
         return len(text)
