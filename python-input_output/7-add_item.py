9ef58108b503b07cd2df980014b7340b148a88a6
knapp <emiliodamasco04@gmail.com>
2023-06-22 07:17:34 -0700

    fixed except

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 try:
     items = load_from_json_file("add_item.json")
-except FileNotFoundError:
+except Exception:
     items = []
 
 if len(sys.argv) != 1:
