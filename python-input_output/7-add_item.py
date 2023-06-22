9a9d06fa43d9cab4bb0a8991d2034640d8da984b
knapp <emiliodamasco04@gmail.com>
2023-06-22 07:18:51 -0700

    fixed variable

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,13 +8,15 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+filename = "add_item.json"
+
 try:
-    items = load_from_json_file("add_item.json")
-except Exception:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
     items = []
 
 if len(sys.argv) != 1:
     for i in range(1, len(sys.argv)):
         items.append(sys.argv[i])
 
-save_to_json_file(items, "add_item.json")
+save_to_json_file(items, filename)
