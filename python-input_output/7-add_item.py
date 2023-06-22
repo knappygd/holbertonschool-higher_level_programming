1cb5cad26d79b4051b38d43ff54773ea36a27ae4
knapp <emiliodamasco04@gmail.com>
2023-06-22 07:20:18 -0700

    fixed perms

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,10 +8,8 @@ import sys
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-filename = "add_item.json"
-
 try:
-    items = load_from_json_file(filename)
+    items = load_from_json_file("add_item.json")
 except FileNotFoundError:
     items = []
 
@@ -19,4 +17,4 @@ if len(sys.argv) != 1:
     for i in range(1, len(sys.argv)):
         items.append(sys.argv[i])
 
-save_to_json_file(items, filename)
+save_to_json_file(items, "add_item.json")
