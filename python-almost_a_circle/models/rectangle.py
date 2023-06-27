1426ef6ae3faaa60227b6fd75e8563c0a156b543
knapp <emiliodamasco04@gmail.com>
2023-06-27 09:37:29 -0700

    added dict

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,12 @@ class Rectangle(Base):
         else:
             for k, v in kwargs.items():
                 setattr(self, k, v)
+
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
