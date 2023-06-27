922c908030ea6ddff4c5749f16ba15effc5b3275
knapp <emiliodamasco04@gmail.com>
2023-06-27 05:38:46 -0700

    added exceptions

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,6 +28,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -36,6 +40,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -44,6 +52,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -52,4 +62,6 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
