80735ad05499ffc9eaf913f7aa91bc8efdb22190
knapp <emiliodamasco04@gmail.com>
2023-06-27 07:09:33 -0700

    fixed square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """ Inits a square. """
 
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id, x, y, size, size)
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
