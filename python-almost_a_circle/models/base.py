e4587d422e9bfe82da83bf5ec3f88da9b9cd390c
knapp <emiliodamasco04@gmail.com>
2023-06-27 09:53:01 -0700

    fixed json

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -19,5 +19,5 @@ class Base:
     def to_json_string(list_dictionaries):
         """ Return JSON representation. """
         if list_dictionaries is None or list_dictionaries == []:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
