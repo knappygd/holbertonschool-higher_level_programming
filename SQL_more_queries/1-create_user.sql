2e17ec7abbbbaa885d6ac83e38b3f66a8f22de1f
knapp <emiliodamasco04@gmail.com>
2023-07-25 07:04:32 -0700

    added set to password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Creates a user.
 
 CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
     ON *.*
     TO 'user_0d_1'@'localhost';
-    IDENTIFIED BY 'user_0d_1_pwd';
 
