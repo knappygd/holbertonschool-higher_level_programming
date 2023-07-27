8e1adbdb041d357edce19306f069ddc53c056b0f
knapp <emiliodamasco04@gmail.com>
2023-07-27 09:23:39 -0700

    added order by

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     )
 
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
 
     for state in cursor.fetchall():
         print(state)
