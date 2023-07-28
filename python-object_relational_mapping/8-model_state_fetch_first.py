3b9b040163a2960b54212afd2b4ef9aa394cd3c7
knapp <emiliodamasco04@gmail.com>
2023-07-28 13:52:02 -0700

    added 8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Contains the class definition of a State and
+an instance Base = declarative_base().
+"""
+from sys import argv
+import sqlalchemy
+from sqlalchemy import create_engine
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
+                           format(argv[1], argv[2], argv[3]))
+
+    con = engine.connect()
+    states = sqlalchemy.select(State.id, State.name).order_by(State.id)
+    res = con.execute(states).first()
+
+    if res:
+        print("{}: {}".format(res[0], res[1]))
+    else:
+        print("Nothing")
